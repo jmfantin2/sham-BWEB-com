b64b891ecbbb92b548ff1317b6c5f5d54fa60de4
JM Fantin <jmfantin@outlook.com>
2025-02-10 12:08:09 -0300

    attempt to scroll page down in mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 // src/app.tsx
-import React from 'react';
+import React, { useEffect } from 'react';
 import { RingLoader } from './components/RingLoader';
 import CompoundPresentation from './components/CompoundPresentation';
 
 const App: React.FC = () => {
+  useEffect(() => {
+    // Scroll to bottom after a short delay to ensure content is rendered
+    window.scrollTo(0, document.documentElement.scrollHeight);
+  }, []);
   return (
     <>
       <RingLoader mode="fullpage" />
